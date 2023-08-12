ca5bae18ddb9102ae07b36cbd0e2e66b1a07a9ee
melchior.moraes <melchior.alencar@gmail.com>
2023-08-12 17:41:49 -0300

    feat(Project): Initial commit project

---
+++ src/main/java/com/diariodeumdev/apiclientspringbootboilerplate/ApiclientSpringBootBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.diariodeumdev.apiclientspringbootboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiclientSpringBootBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiclientSpringBootBoilerplateApplication.class, args);
+	}
+
+}
